debabbe56401f665d77d7a4544792d486a2f67b6
Jupiter82 <79780639+Jupiter82@users.noreply.github.com>
2025-01-13 17:59:15 +0545

    image update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
         </button>
       </Link>
 
-      <Link to="/context">
+      <Link to="/imageList">
         <button
           type="button"
           className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-xs px-3 py-2 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
